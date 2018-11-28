468cc08b80dbda4acf6b882d8a4e0f7789979a6a
Dani Ortiz <daniortizgar@gmail.com>
2018-11-28 15:00:41 +0100

    improved pagination

--- angular6/src/app/home/home.component.ts
+++ angular6/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { VideoListConfig, UserService, VideosService, Video, LabelsService } from '../core';
@@ -25,8 +25,14 @@ export class HomeComponent implements OnInit {
   labels: Array<string> = [];
   labelsLoaded = false;
   video: Video;
-
+  limit = 6;
+@HostListener('window:resize') onResize(event){
+  this.changeLimit(window.innerWidth);
+}
   ngOnInit() {
+    
+    this.changeLimit(window.innerWidth);
+
     this.userService.isAuthenticated.subscribe(
       (authenticated) => {
         this.isAuthenticated = authenticated;
@@ -61,5 +67,24 @@ export class HomeComponent implements OnInit {
 
     // Otherwise, set the list object
     this.listConfig = {type: type, filters: filters};
+  }
+
+  changeLimit(size){
+    if(size <= 910 && this.limit != 2){
+      this.limit = 2;
+      this.setListTo('all',{});
+    }else if(size <= 1200 && size > 910 && this.limit != 3){
+      this.limit = 3;
+      this.setListTo('all',{});
+    }else if(size <= 1490 && size > 1200 && this.limit != 4){
+      this.limit = 4;
+      this.setListTo('all',{});
+    }else if(size <= 1785 && size > 1490 && this.limit != 5){
+      this.limit = 5;
+      this.setListTo('all',{});
+    }else if(size > 1785 && this.limit <= 5){
+      this.limit = 6;
+      this.setListTo('all',{});
+    }
   }
 }
