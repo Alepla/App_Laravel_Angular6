ccfecb6583b3f863370ed5fd93ea551a62babbab
Dani Ortiz <daniortizgar@gmail.com>
2018-12-14 18:28:42 +0100

    Finished basic search

--- angular6/src/app/search/search.component.ts
+++ angular6/src/app/search/search.component.ts
@@ -11,6 +11,9 @@ import { ActivatedRoute } from '@angular/router';
 export class SearchComponent implements OnInit {
     videos: Video;
     users: User;
+    userVideos: Video;
+    uniqueUser: User;
+    nameuser = "";
     listConfig: VideoListConfig = {
         type: 'all',
         filters: {},
@@ -24,8 +27,43 @@ export class SearchComponent implements OnInit {
     ngOnInit() {
         this.route.data.subscribe(
             (search) => {
-                this.videos = search.search.videos.original.searches;
+                let val;
+                let arr = [];
+                
+                if(!search.search.userVideos.original.searches)
+                    search.search.userVideos.original.searches = [];
+                this.userVideos = search.search.userVideos.original.searches;
+                
+                this.uniqueUser = search.search.users.original.searches[0];
+
+                if(search.search.users.original.searches[0])
+                    this.nameuser = search.search.users.original.searches[0].username;
+                else
+                    this.nameuser = "";
+
+                search.search.users.original.searches.forEach((element,key) => {
+                    if(key > 0)
+                        arr.push(element);   
+                });
+                
+                search.search.users.original.searches = arr;
                 this.users = search.search.users.original.searches;
+                arr = [];
+
+                if(search.search.userVideos.original.searches.length > 0){
+                    console.log(search.search.userVideos.original.searches.length)
+                    search.search.videos.original.searches.forEach(element => {
+                        val = search.search.userVideos.original.searches.find(({id}) => element.id === id);
+                        if(!val){
+                            arr.push(element);
+                        }
+                    });
+                
+                    search.search.videos.original.searches = arr;
+                }else{
+                    this.userVideos = null;
+                }
+                this.videos = search.search.videos.original.searches;
             }
         );
     }
--- laravel/app/Http/Controllers/Api/SearchController.php
+++ laravel/app/Http/Controllers/Api/SearchController.php
@@ -15,11 +15,21 @@ class SearchController extends ApiController
     }
 
     public function index(Request $request){
+        $userVideos = null;
         $videos = Video::where('title', 'like', "%" . $request->input("filter") . "%")
-                        ->  orWhere('description', 'like', "%" . $request->input("filter") . "%")->get();
-        $users = User::where('username', 'like', "%" . $request->input("filter") . "%")->get(); 
+                         ->  orWhere('description', 'like', "%" . $request->input("filter") . "%")->get();
+        $users = User::where('username', 'like', "%" . $request->input("filter") . "%")->orderBy('followers', 'desc')->get();
+
+        if($users != null){
+            foreach ($users as $key => $user) {
+                if($key < 1)
+                    $userVideos = Video::where('user_id', '=', $user->id)->orderBy('created_at', 'desc')->get();
+            }
+        }
+
         $search['videos'] = $this->respondWithTransformer($videos);
         $search['users'] = $this->respondWithTransformer($users);
+        $search['userVideos'] = $this->respondWithTransformer($userVideos);
 
         return $search;
     }
